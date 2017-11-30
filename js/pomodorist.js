85eb322404f695f12edbfa6424715c56ab124048
penomilan <penomilan@gmail.com>
2017-11-30 21:11:24 +0100

    Aded basic html

--- js/pomodorist.js
+++ js/pomodorist.js
@@ -1,1 +1,18 @@
+var n=25;
 
+function counter() {
+   var s = document.getElementById("count");
+      s.innerHTML = n;
+       n--;
+   if (s < 0) {
+       num = 25;
+       numS--;
+   }
+   if (n < 0){
+       n = 25;
+       n--;
+   }
+    
+}
+
+setInterval(count, 100);
