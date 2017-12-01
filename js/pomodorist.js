a3eee0030d7e6771f5177be787816b7d9ca8b26c
MilanPeno <MilanPeno@MilanPeno.local>
2017-12-01 22:59:00 +0100

    update

--- js/pomodorist.js
+++ js/pomodorist.js
@@ -1,18 +1,14 @@
-var n=25;
-
 function counter() {
-   var s = document.getElementById("count");
-      s.innerHTML = n;
-       n--;
-   if (s < 0) {
-       num = 25;
-       numS--;
+  var n=1500;
+function counter1() {
+   if (n < 0) {
+    location.reload();
+    alert("YOU NEED A BREAK!");
+      
    }
-   if (n < 0){
-       n = 25;
+   
+document.getElementById("count").innerHTML=n;
        n--;
-   }
-    
+     }
+window.setInterval(counter1, 1000);
 }
-
-setInterval(count, 100);
